576844e0d6e7519eac4cc0e5cbe9c89537b6bd13
nurmd7228@gmail.com <nurmd7228@gmail.com>
2024-05-29 19:30:43 +0600

    add new

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  const app = express();
  const cors = require('cors');
  const port = process.env.PORT || 5000;
+ const jwt = require('jsonwebtoken')
 
  //middleware
  app.use(cors());
@@ -30,9 +31,60 @@ async function run() {
     const usersCollection = client.db('Bistro_Boss').collection('users');
 
 
+    // middleeares
+    const verityToken = (req,res, next)=>{
+      console.log(req.body)
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorized access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN, (error, decode)=>{
+        if(error){
+          return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decode;
+        next();
+      })
+      // next();
+    }
     // user releated api
+    app.get('/users', verityToken, async(req, res)=>{
+      console.log(req)
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    // User delete 
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // User update rule 
+    app.patch('/user/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc ={
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
     app.post ('/users', async(req,res)=>{
       const user = req.body;
+      // if user exsited 
+     
+      const query = {email: user.email};
+      const existUser = await usersCollection.findOne(query);
+      if(existUser){
+        return res.send({acknowledged: false, message: 'User already exist'});
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
@@ -66,6 +118,14 @@ async function run() {
       const query = {_id: new ObjectId(id)};
       const result = await cartsCollection.deleteOne(query);
       res.send(result)
+    })
+
+
+    // jwt releted api 
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+      res.send({token});
     })
 
 
